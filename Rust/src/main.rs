27ab34f9359b5d87dda7c7b7704726fe56f383de
이인수 <in63119@gmail.com>
2023-12-18 12:49:23 +0900

    Study and implement crate usage in Rust

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,3 +1,18 @@
+use sha2::{Sha256, Digest};
+
 fn main() {
-    println!("Hello, world!");
+    // 해싱할 문자열
+    let data = "Hello, world!";
+
+    // SHA-256 객체 생성
+    let mut hasher = Sha256::new();
+
+    // 데이터 업데이트
+    hasher.update(data);
+
+    // 해시 계산 완료
+    let result = hasher.finalize();
+
+    // 결과 출력
+    println!("SHA-256 hash of '{}' is {:?}", data, result);
 }
